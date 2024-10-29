b8321b5e98b6ffcb1989531dc81ba379a355c528
Néstor Valenzuela <nes.valenzuela@duocuc.cl>
2024-10-29 17:07:32 -0300

    "Ignorar archivo en el control de versiones"

--- Desarrollo/backend/src/config/jwtConfig.js
+++
@@ -1,5 +0,0 @@
-export const JWT_SECRET = process.env.JWT_SECRET || "x5HJM9k#mP2$vL8nQ3@zR7";
-export const JWT_REFRESH_SECRET =
-  process.env.JWT_REFRESH_SECRET || "bN9#kL5$pX2@mR4&vZ7*wY3";
-export const JWT_EXPIRES_IN = "10m";
-export const JWT_REFRESH_EXPIRES_IN = "7d";
